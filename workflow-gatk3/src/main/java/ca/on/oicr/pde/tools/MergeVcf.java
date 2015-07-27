58a657726f7b48c8fe4d5a2be5de32e9df512e1e
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2015-07-27 16:07:22 -0400

    Add license info

--- workflow-gatk3/src/main/java/ca/on/oicr/pde/tools/MergeVcf.java
+++ workflow-gatk3/src/main/java/ca/on/oicr/pde/tools/MergeVcf.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright (C) 2015 Ontario Institute of Cancer Research
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us:
+ *
+ * Ontario Institute for Cancer Research
+ * MaRS Centre, West Tower
+ * 661 University Avenue, Suite 510
+ * Toronto, Ontario, Canada M5G 0A3
+ * Phone: 416-977-7599
+ * Toll-free: 1-866-678-6427
+ * www.oicr.on.ca
+ *
+ */
 package ca.on.oicr.pde.tools;
 
 import ca.on.oicr.pde.tools.common.AbstractCommand;
@@ -59,12 +86,12 @@ public class MergeVcf extends AbstractCommand {
             } else {
                 outputFilePath = outputDir + RandomStringUtils.randomAlphanumeric(4) + ".merged.vcf";
             }
-            
+
             List<String> c = new LinkedList<>();
-            
+
             c.add(perl);
             c.add(mergeScriptPath);
-            
+
             for(String inputFile : inputFiles){
                 c.add("--vcf-input-file");
                 c.add(inputFile);
@@ -72,7 +99,7 @@ public class MergeVcf extends AbstractCommand {
 
             c.add("--vcf-output-file");
             c.add(outputFilePath);
-            
+
             MergeVcf cmd = new MergeVcf();
             cmd.command.addAll(c);
             cmd.outputFile = outputFilePath;
