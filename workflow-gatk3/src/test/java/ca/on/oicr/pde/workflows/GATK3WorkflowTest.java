94a104dcc900083764d7b26e7a945b4b21cf12dd
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2015-09-22 16:41:55 -0400

    Update workflows to use DryRun test method

--- workflow-gatk3/src/test/java/ca/on/oicr/pde/workflows/GATK3WorkflowTest.java
+++ workflow-gatk3/src/test/java/ca/on/oicr/pde/workflows/GATK3WorkflowTest.java
@@ -27,21 +27,21 @@
  */
 package ca.on.oicr.pde.workflows;
 
+import ca.on.oicr.pde.testing.workflow.DryRun;
+import ca.on.oicr.pde.testing.workflow.TestDefinition;
 import ca.on.oicr.pde.workflows.GATK3Workflow.VariantCaller;
 import static ca.on.oicr.pde.workflows.GATK3Workflow.VariantCaller.HAPLOTYPE_CALLER;
 import static ca.on.oicr.pde.workflows.GATK3Workflow.VariantCaller.UNIFIED_GENOTYPER;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Sets;
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
-import static net.sourceforge.seqware.pipeline.workflowV2.MockWorkflowDataModelFactory.buildWorkflowModel;
+import net.sourceforge.seqware.pipeline.workflowV2.AbstractWorkflowDataModel;
 import net.sourceforge.seqware.pipeline.workflowV2.model.AbstractJob;
 import net.sourceforge.seqware.pipeline.workflowV2.model.SqwFile;
 import org.apache.commons.io.FileUtils;
@@ -61,37 +61,44 @@ public class GATK3WorkflowTest {
     public GATK3WorkflowTest() {
     }
 
-    private GATK3Workflow getWorkflowClientObject(Map<String, String> config) throws IOException, IllegalAccessException {
-        GATK3Workflow w = new GATK3Workflow();
-        w.setConfigs(config);
-        buildWorkflowModel(System.getProperty("bundleDirectory"), w);
-        return w;
+    @Test
+    public void dryRunRegressionTests() throws Exception {
+        TestDefinition td = TestDefinition.buildFromJson(FileUtils.readFileToString(new File("src/test/resources/developmentTests.json")));
+
+        for (TestDefinition.Test t : td.getTests()) {
+            //disable calculating chr_sizes from interval_files
+            Map<String, String> params = new HashMap<>(t.getParameters());
+            if (!params.containsKey("chr_sizes")) {
+                params.put("chr_sizes", "");
+            }
+
+            DryRun d = new DryRun(System.getProperty("bundleDirectory"), params, GATK3Workflow.class);
+            d.buildWorkflowModel();
+            d.validateWorkflow();
+        }
     }
 
     @Test(expectedExceptions = java.lang.AssertionError.class)
-    public void checkForNull() throws IOException, IllegalAccessException {
-        Map<String, String> config = getDefaultConfig();
+    public void checkForNull() throws Exception {
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "null");
         config.put("input_files", "/data/test/PCSI0022P.val.bam,/data/test/PCSI0022R.val.bam,/data/test/PCSI0022X.val.bam,/data/test/PCSI0022C.val.bam,"
                 + "/data/test/PCSI0022P.val.bai,/data/test/PCSI0022R.val.bai,/data/test/PCSI0022X.val.bai,/data/test/PCSI0022C.val.bai");
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
-        GATK3Workflow w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
     }
 
     @Test(enabled = true)
-    public void checkJobNumber() throws IOException, IllegalAccessException {
-        Map<String, String> config;
-        GATK3Workflow w;
-
-        config = getDefaultConfig();
+    public void checkJobNumber() throws Exception {
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", "/data/test/PCSI0022P.val.bam,/data/test/PCSI0022R.val.bam,/data/test/PCSI0022X.val.bam,/data/test/PCSI0022C.val.bam,"
                 + "/data/test/PCSI0022P.val.bai,/data/test/PCSI0022R.val.bai,/data/test/PCSI0022X.val.bai,/data/test/PCSI0022C.val.bai");
         config.put("variant_caller", "Unified_GeNoTyPeR");
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
         config.put("chr_sizes", "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM");
-        w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
 
         Assert.assertEquals(w.getWorkflow().getJobs().size(), 206);
@@ -114,7 +121,7 @@ public class GATK3WorkflowTest {
     }
 
     @Test(enabled = true)
-    public void splitByChromosome() throws IOException, IllegalAccessException {
+    public void splitByChromosome() throws Exception {
 
         int numInputFiles = 200;
         List<String> inputFiles = new LinkedList<>();
@@ -124,9 +131,7 @@ public class GATK3WorkflowTest {
             inputFiles.add(fileName + ".bai");
         }
 
-        Map<String, String> config;
-        GATK3Workflow w;
-        config = getDefaultConfig();
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", StringUtils.join(inputFiles, ","));
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
@@ -138,7 +143,7 @@ public class GATK3WorkflowTest {
         int parallelismLevel = Arrays.asList(StringUtils.split(config.get("chr_sizes"), ",")).size();
 
         //haplotype caller
-        w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
         Assert.assertEquals(w.getWorkflow().getJobs().size(),
                 getExpectedJobCount(numInputFiles, parallelismLevel, Sets.newHashSet(VariantCaller.HAPLOTYPE_CALLER), true));
@@ -158,7 +163,7 @@ public class GATK3WorkflowTest {
                 getExpectedJobCount(numInputFiles, parallelismLevel, Sets.newHashSet(VariantCaller.HAPLOTYPE_CALLER, VariantCaller.UNIFIED_GENOTYPER), true));
 
         //reset
-        config = getDefaultConfig();
+        config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", StringUtils.join(inputFiles, ","));
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
@@ -190,11 +195,9 @@ public class GATK3WorkflowTest {
     }
 
     @Test(enabled = true)
-    public void noSplit() throws IOException, IllegalAccessException {
+    public void noSplit() throws Exception {
 
-        Map<String, String> config;
-        GATK3Workflow w;
-        config = getDefaultConfig();
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", "/test/1.bam,/test/1.bai");
         config.put("chr_sizes", "");
@@ -202,7 +205,7 @@ public class GATK3WorkflowTest {
 
         //unified gentotyper
         config.put("variant_caller", "unified_genotyper");
-        w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
         Assert.assertEquals(w.getWorkflow().getJobs().size(),
                 GATK3WorkflowTest.this.getExpectedJobCount(1, 0, VariantCaller.UNIFIED_GENOTYPER, true));
@@ -223,10 +226,8 @@ public class GATK3WorkflowTest {
     }
 
     @Test
-    public void extraParamSuccessTest() throws IOException, IllegalAccessException {
-        Map<String, String> config;
-        GATK3Workflow w;
-        config = getDefaultConfig();
+    public void extraParamSuccessTest() throws Exception {
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", "/test/1.bam,/test/1.bai");
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
@@ -238,20 +239,18 @@ public class GATK3WorkflowTest {
         config.put("gatk_haplotype_caller_params", "--param1 --param2");
         config.put("gatk_unified_genotyper_params", "--param1 --param2");
         config.put("chr_sizes", "chr1,chr2,chr3,chr4,chr5,chr6,chr7,chr8,chr9,chr10,chr11,chr12,chr13,chr14,chr15,chr16,chr17,chr18,chr19,chr20,chr21,chr22,chrX,chrY,chrM");
-        w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
     }
 
     @Test(expectedExceptions = java.lang.AssertionError.class)
-    public void extraParamFailureTest() throws IOException, IllegalAccessException {
-        Map<String, String> config;
-        GATK3Workflow w;
-        config = getDefaultConfig();
+    public void extraParamFailureTest() throws Exception {
+        Map<String, String> config = new HashMap<>();
         config.put("identifier", "gatk.ex");
         config.put("input_files", "/test/1.bam,/test/1.bai");
         config.put("gatk_dbsnp_vcf", "/tmp/dbsnp.vcf");
         config.put("gatk_realigner_target_creator_params", "--param1--param2");
-        w = getWorkflowClientObject(config);
+        AbstractWorkflowDataModel w = getWorkflowClientObject(config);
         validateWorkflow(w);
     }
 
@@ -292,7 +291,7 @@ public class GATK3WorkflowTest {
         return numJobs;
     }
 
-    private void validateWorkflow(GATK3Workflow w) {
+    private void validateWorkflow(AbstractWorkflowDataModel w) {
 
         //check for null string
         for (AbstractJob j : w.getWorkflow().getJobs()) {
@@ -344,11 +343,11 @@ public class GATK3WorkflowTest {
         }
     }
 
-    private Map<String, String> getDefaultConfig() throws IOException {
-        Properties p = new Properties();
-        File defaults = new File(System.getProperty("bundleDirectory") + "/config/defaults.ini");
-        p.load(FileUtils.openInputStream(defaults));
-        return new HashMap<>((Map) p);
+    private AbstractWorkflowDataModel getWorkflowClientObject(Map<String, String> config) throws Exception {
+        DryRun d = new DryRun(System.getProperty("bundleDirectory"), config, GATK3Workflow.class);
+        AbstractWorkflowDataModel w = d.buildWorkflowModel();
+        d.validateWorkflow();
+        return w;
     }
 
 }
